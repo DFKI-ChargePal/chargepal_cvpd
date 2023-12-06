377f901904b13eaf3f68b995fab6734d96285f84
Georg John <georg.john@dfki.de>
2023-12-06 16:37:36 +0100

    Adjust detector class

--- cvpd/detector/detector_aruco_marker.py
+++ cvpd/detector/detector_aruco_marker.py
@@ -4,15 +4,17 @@ from __future__ import annotations
 import cv2 as cv
 import numpy as np
 from pathlib import Path
+from camera_kit import converter
 from camera_kit import DetectorBase
+from scipy.spatial.transform import Rotation as R
 
 # local
 from cvpd.utilities import rotate_rot_vec
 from cvpd.geo_pattern.aruco_marker import ArucoMarker
 
 # typing
-from typing import Any
 from numpy import typing as npt
+from camera_kit.core import PosOrinType
 
 
 class ArucoMarkerDetector(DetectorBase):
@@ -20,51 +22,44 @@ class ArucoMarkerDetector(DetectorBase):
     def __init__(self, config_file: str | Path):
         # Read configuration via base class
         super().__init__(config_file)
-        # Get searched geometric pattern
-        obj_cfg: dict[str, dict[str, Any]] | None = self.config.get('object_config')
-        if obj_cfg is None:
-            raise RuntimeError(f"Can't find configuration values for aruco markers.")
-        self.obj_marker: dict[str, ArucoMarker] = {}
-        for obj_name, obj_val in obj_cfg.items():
-            self.obj_marker[obj_name] = ArucoMarker(
-                aruco_id=obj_val['marker_id'],
-                aruco_size_mm=obj_val['marker_size'],
-                aruco_type=obj_val['marker_type']
-            )
+        # Get marker configuration
+        marker_id = self.config['marker_id']
+        marker_type = self.config['marker_type']
+        marker_size = self.config['marker_size']
+        marker_offset = self.config['marker_offset']
+        self.marker = ArucoMarker(
+            marker_id, marker_size, marker_type, marker_offset['xyz'], marker_offset['wxyz'])
 
-    def _find_pose(self, object_name: str) -> tuple[bool, npt.NDArray[np.float_]]:
+    def _find_pose(self) -> tuple[bool, PosOrinType]:
         """ Finding the pose of a marker described object
 
-        Args:
-            object_name: Unique name of the searched object type
-
         Returns:
-            (True if pose was found; Pose as numpy array. Containing the rotation and translation vector)
+            (True if pose was found; Pose containing position [xyz] and quaternion [wxyz] vector)
         """
         # Initialize return variables with default values
-        pose_found = False
-        r_vec, t_vec = np.array([], dtype=np.float_), np.array([], dtype=np.float_)
-        # Find marker configuration
-        aruco_marker = self.obj_marker.get(object_name)
-        if aruco_marker is not None:
-            img = self.camera.get_color_frame()
-            # get all markers on image
-            found_corners, found_ids, _ = aruco_marker.cv_detector.detectMarkers(img)
-            # if corners are detected, check if they include the searched marker
-            if len(found_corners) > 0:
-                det_id_list = [_id[0] for _id in found_ids.tolist()]
-                if aruco_marker.aruco_id in det_id_list:
-                    # Get first index
-                    id_idx = det_id_list.index(aruco_marker.aruco_id)
-                    corners = found_corners[id_idx].reshape((4, 2))
-                    # estimate pose for the single marker
-                    pose_found, r_vec, t_vec = self._estimate_pose_single_marker(corners, aruco_marker.obj_pts_marker)
-        return pose_found, np.asarray((r_vec, t_vec))
+        found = False
+        p, q = (0.0, 0.0, 0.0), (1.0, 0.0, 0.0, 0.0)
+        img = self.camera.get_color_frame()
+        # get all markers on image
+        found_corners, found_ids, _ = self.marker.cv_detector.detectMarkers(img)
+        # if corners are detected, check if they include the searched marker
+        if len(found_corners) > 0:
+            det_id_list = [_id[0] for _id in found_ids.tolist()]
+            if self.marker.aruco_id in det_id_list:
+                # Get first index
+                id_idx = det_id_list.index(self.marker.aruco_id)
+                corners = found_corners[id_idx].reshape((4, 2))
+                # estimate pose for the single marker
+                found, pq = self._estimate_pose_single_marker(corners, self.marker.obj_pts_marker)
+                if found:
+                    p = tuple((np.reshape(pq[0], 3) + np.reshape(self.marker.offset_p, 3)).tolist())
+                    q = tuple((R.from_quat(pq[1]) * R.from_quat(self.marker.offset_q)).as_quat().tolist())
+        return found, (p, q)
 
     def _estimate_pose_single_marker(self,
                                      marker_corners: npt.NDArray[np.float_],
                                      marker_obj_pts: npt.NDArray[np.float_]
-                                     ) -> tuple[bool, npt.NDArray[np.float_], npt.NDArray[np.float_]]:
+                                     ) -> tuple[bool, PosOrinType]:
         """ Method to estimate the pose of a single ArUco marker.
 
         Args:
@@ -73,7 +68,7 @@ class ArucoMarkerDetector(DetectorBase):
         Returns:
             The rotation and translation vector of the marker pose
         """
-        _ret, r_vec, t_vec = cv.solvePnP(
+        found, r_vec, t_vec = cv.solvePnP(
             marker_obj_pts,
             marker_corners,
             self.camera.cc.intrinsic,
@@ -89,5 +84,6 @@ class ArucoMarkerDetector(DetectorBase):
             tvec=t_vec,
             criteria=(cv.TermCriteria_EPS + cv.TermCriteria_COUNT, 30, 0.001)
             )
-        r_vec = rotate_rot_vec(r_vec, 'x', np.pi)
-        return _ret, r_vec, t_vec
+        r_vec = np.squeeze(rotate_rot_vec(r_vec, 'z', -np.pi/2))
+        pq = converter.cv_to_pq(r_vec, t_vec)
+        return found, pq
--- cvpd/geo_pattern/aruco_marker.py
+++ cvpd/geo_pattern/aruco_marker.py
@@ -7,15 +7,22 @@ import numpy as np
 # local
 from cvpd.geo_pattern._aruco_types import ARUCO_DICT
 
+# typing
+from numpy import typing as npt
+
 
 LOGGER = logging.getLogger(__name__)
 
 
 class ArucoMarker:
 
-    def __init__(self, aruco_id: int, aruco_size_mm: int, aruco_type: str) -> None:
-        # TODO: Check if aruco_id is valid
-        self.aruco_id = aruco_id
+    def __init__(self,
+                 aruco_id: int,
+                 aruco_size_mm: int,
+                 aruco_type: str,
+                 marker_offset_xyz: npt.ArrayLike | None = None,
+                 marker_offset_wxyz: npt.ArrayLike | None = None,
+                 ) -> None:
         self.ar_type_dict = ARUCO_DICT.get(aruco_type)
         if self.ar_type_dict is None:
             error_msg = f"No AR tag with key '{aruco_type}' found"
@@ -23,6 +30,14 @@ class ArucoMarker:
             raise KeyError(error_msg)
         self.aruco_dict = cv.aruco.getPredefinedDictionary(self.ar_type_dict)
         self.cv_detector = cv.aruco.ArucoDetector(self.aruco_dict)
+        # Check if aruco_id is valid
+        if 0 <= aruco_id < self.id_range:
+            self.aruco_id = aruco_id
+        else:
+            raise ValueError(f"Given id of ArUco marker '{aruco_id}' not in valid range 0...{self.id_range - 1}")
+        # Store offset as numpy array
+        self.offset_p = np.array(marker_offset_xyz)
+        self.offset_q = np.array(marker_offset_wxyz)
         # added object points
         ar_size_m_2 = aruco_size_mm / 2 / 1000
         self.obj_pts_marker = np.array([
@@ -31,3 +46,7 @@ class ArucoMarker:
             [+ar_size_m_2, -ar_size_m_2, 0.0],
             [-ar_size_m_2, -ar_size_m_2, 0.0],
         ], dtype=np.float_)
+
+    @property
+    def id_range(self) -> int:
+        return int(self.aruco_dict.bytesList.shape[0])
