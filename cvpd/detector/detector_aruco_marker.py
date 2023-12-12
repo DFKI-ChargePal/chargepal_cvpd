ab094f72047be4ba2c27c9839b608d1f12097035
Georg John <georg.john@dfki.de>
2023-12-12 13:50:45 +0100

    Remove rotation correction

--- cvpd/detector/detector_aruco_marker.py
+++ cvpd/detector/detector_aruco_marker.py
@@ -91,6 +91,6 @@ class ArucoMarkerDetector(DetectorBase):
             tvec=t_vec,
             criteria=(cv.TermCriteria_EPS + cv.TermCriteria_COUNT, 30, 0.001)
             )
-        r_vec = np.reshape(rotate_rot_vec(r_vec, 'x', np.pi), 3)
+        # r_vec = np.reshape(rotate_rot_vec(r_vec, 'x', np.pi), 3)
         pq = converter.cv_to_pq(r_vec, t_vec)
         return found, pq
